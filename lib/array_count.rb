75516c8831b0d0fd0434a1413b4878d42533cee7
Aian Soktoev <soktoevayan@gmail.com>
2020-06-01 23:25:08 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,33 +1,14 @@
-#a = [1,2,3,4,'','wdw','wdwdwdw',1,'','','','','']
-
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
-  i=0 
-  newArr =[]
-  while i<array.length do
-    if array[i].class == String
-      newArr.push(array[i])
-    end
-    i+=1 
-  end 
-  return newArr.count 
-end
-
-#count_strings(a)
+  array.count do |element|
+    element.class == String 
+  end   
+end  
 
 
 
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
-  i=0 
-  count = 0 
-  while i<array.length do
-    if array[i]=="" 
-      count+=1 
-    end
-    i+=1 
-  end 
-  return count 
-end 
+  array.count do |element|
+    element ==''
+  end   
+end     
 
-#count_empty_strings(a)
